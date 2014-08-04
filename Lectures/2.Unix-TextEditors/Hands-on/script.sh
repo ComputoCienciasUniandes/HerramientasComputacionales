d9a4282a98079d93fd7228e9202d9dcf340a4673
jngaravitoc <nico.garavito@gmail.com>
2014-08-04 18:04:32 -0500

    ordering

--- Lectures/2.Unix-TextEditors/Hands-on/script.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw2.github.com/jngaravitoc/HerramientasComputacionales/master/2.Unix-TextEditors/Hands-on/metamorphosis.txt
-
-grep -o -i "a" metamorphosis.txt 
--- Lectures/2.Unix-TextEditors/Hands-on/script.sh~
+++
@@ -1,1 +0,0 @@
-wget https://raw2.github.com/jngaravitoc/HerramientasComputacionales/master/2.Unix-TextEditors/Hands-on/metamorphosis.txt
