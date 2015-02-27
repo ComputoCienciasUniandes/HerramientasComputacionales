bd1f1a8e122be8dd133d66721cae79ab0a85da05
davidnet <david.cardozo@me.com>
2015-02-27 03:48:37 -0500

    added exercises and presentation

---
+++ Lectures/04.Python-1/Material de Apoyo/Listings/Listing7.py
@@ -0,0 +1,19 @@
+
+mi_nombre = 'David Cardozo'
+mi_edad = 19 #
+mi_altura = 170 # cms
+mi_peso = 180 # lbs
+mis_ojos = 'negros'
+mis_dientes = 'blancos'
+mi_cabello = 'cafe'
+	
+print "Vamos hablar de %s." % mi_nombre
+print "El es % d centimetros de alto" % mi_altura
+print "El tiene % d libras de peso." % mi_peso
+print "Soy bien pesado"
+print "El tiene ojos de color %s  y cabello %s ." % (mis_ojos, mi_cabello)
+print "Sus dientes %s dependen del cafe." % mis_dientes
+	
+
+print "Si yo sumo % d, % d, y % d obtengo % d." % (
+mi_edad, mi_altura, mi_peso, mi_edad + mi_altura + mi_peso)
